11a1312695e6a7481e1b0284e2dfa6a233d4ba52
Morgan Newman <mpnewman95@gmail.com>
2018-09-18 13:17:08 -0700

    Added POST / support

--- routes/notes.js
+++ routes/notes.js
@@ -60,18 +60,40 @@ router.get('/:id', (req, res, next) => {
       return disconnect();
     })
     .catch(err => next(err));
-
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
-
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({
-    id: 2,
-    title: 'Temp 2'
-  });
-
+  const requiredFields = ['title'];
+  const availableFields = ['title', 'content'];
+  // Validate that all required fields are present
+  for (const field of requiredFields) {
+    if (!(field in req.body)) {
+      const err = new Error(`Missing ${field} in request body.`);
+      err.status = 400;
+      next(err);
+    }
+  }
+  // Construct the new DB document
+  const newNote = {};
+  for (const field of availableFields) {
+    if (field in req.body) {
+      newNote[field] = req.body[field];
+    }
+  }
+  connect()
+    .then(() => {
+      return Note.create(newNote);
+    })
+    .then(result => {
+      // Verify that a result is returned (otherwise throw 500 error)
+      if (!result) throw new Error();
+      else return res.status(201).json(result);
+    })
+    .then(() => {
+      return disconnect();
+    })
+    .catch(err => next(err));
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
