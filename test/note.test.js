e5a41995f0b6bc624136cf26af8cc91de0db4605
Morgan Newman <mpnewman95@gmail.com>
2018-09-19 13:32:12 -0700

    Adds tests for GET /api/notes

--- test/note.test.js
+++ test/note.test.js
@@ -19,8 +19,8 @@ chai.use(chaiHttp);
 describe('Note Router Tests', () => {
   before(function() {
     return mongoose.connect(TEST_MONGODB_URI, {
-        useNewUrlParser: true
-      })
+      useNewUrlParser: true
+    })
       .then(() => mongoose.connection.db.dropDatabase());
   });
 
@@ -35,8 +35,21 @@ describe('Note Router Tests', () => {
   after(function() {
     return mongoose.disconnect();
   });
+  const validateFields = (res, expectedFields) => {
+    const response = res.body;
+    if (typeof response === 'object') {
+      if (Array.isArray(response)) {
+        for (const item of response) {
+          expect(item).to.have.keys(expectedFields);
+        }
+      }
+      else expect(response).to.have.keys(expectedFields);
+    }
+  };
+  const expectedFields = ['id', 'title', 'content', 'createdAt', 'updatedAt'];
 
   describe('GET /api/notes', function() {
+
     it('should respond with all notes', () => {
       // 1) Call the database **and** the API
       // 2) Wait for both promises to resolve using `Promise.all`
@@ -52,10 +65,33 @@ describe('Note Router Tests', () => {
           expect(res.body).to.have.length(data.length);
         });
     });
+
+    it('should return the correct fields for each item', () => {
+      return chai.request(app).get('/api/notes')
+        .then(res => {
+          validateFields(res, expectedFields);
+        });
+    });
+
+    it('should be able to search for notes (case-insensitive)', () => {
+      chai.request(app).get('/api/notes?searchTerm=GAGA') 
+        .then(res => {
+          expect(res.body.title).to.be('7 things Lady Gaga has in common with cats');
+        });
+    });
+
+    it('should return an empty array with an invalid search', () => {
+      chai.request(app).get('/api/notes?searchTerm=INVALIDDDDDDD') 
+        .then(res => {
+          expect(res.body).to.deep.equal([]);
+        });
+    });
+
   });
 
   describe('GET /api/notes/:id', function() {
-    it('should return correct note', function() {
+
+    it('should return correct note by the `id` parameter', function() {
       let data;
       // 1) First, call the database
       return Note.findOne()
@@ -79,6 +115,13 @@ describe('Note Router Tests', () => {
           expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
         });
     });
+
+    // Should have valid fields
+
+    it('should make sure the `id` parameter is a valid ID', () => {});
+
+    it('should 404 if the ID is valid but does not exist in the database', () => {});
+
   });
 
   describe('POST /api/notes', function() {
@@ -112,5 +155,39 @@ describe('Note Router Tests', () => {
           expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
         });
     });
+
+    // Validate fields
+
+    it('should require a title in the request body', () => {});
+
+    it('should return a valid location header with new ID', () => {});
+
   });
+
+  describe('PUT /api/notes/:id', () => {
+
+    it('should update a note by an `id`', () => {});
+
+    it('should be able to update a single field', () => {});
+    
+    it('should require a valid id', () => {});
+
+    it('should 404 if the id is valid but does not exist in the database', () => {});
+
+    it('should require an id in request body', () => {});
+
+    it('should require a matching id in both the request query and body', () => {});
+
+  });
+
+  describe('DELETE /api/notes/:id', () => {
+
+    it('should delete a note by an `id`', () => {});
+
+    it('should require a valid id', () => {});
+
+    it('should 404 if the id is valid but does not exist in the database', () => {});
+
+  });
+
 });
