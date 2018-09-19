c1415e8108ac3ee2c7df30c9b782abe72fed519f
Morgan Newman <mpnewman95@gmail.com>
2018-09-19 11:35:34 -0700

    Third attempt: integrating Travis-CI

--- server.js
+++ server.js
@@ -44,18 +44,20 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to DB and Listen for incoming connections
-mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error('\n === Did you remember to start `mongod`? === \n');
+if (require.main === module) {
+  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
     console.error(err);
   });
-
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+}
 
 
 module.exports = app; // Export for testing
