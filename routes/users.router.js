5d6e7708cb44e4ec86406b0b362289f2b68a7507
Morgan Newman <mpnewman95@gmail.com>
2018-09-24 09:22:14 -0700

    Adds POST /api/users

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const { requireFields, constructLocationHeader } = require('../utils/route-middleware');
 
-router.post('/users', (req, res, next) => {
-  
+router.post('/users', requireFields(['fullName', 'username', 'password']), (req, res, next) => {
+  const newUser = {
+    fullName: req.body.fullName,
+    username: req.body.username,
+    password: req.body.password
+  };
+  return User.create(newUser)
+    .then(user => res
+      .status(201)
+      .location(constructLocationHeader(req, user))
+      .json(user))
+    .catch(err => next(err));
 });
 
 module.exports = router;
