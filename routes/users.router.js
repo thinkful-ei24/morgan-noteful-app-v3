74fc4b66418bcdb5b1e2d5b92f72a8653994169c
Morgan Newman <mpnewman95@gmail.com>
2018-09-24 11:47:56 -0700

    Adds password hashing to POST /api/users

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,12 +3,16 @@ const User = require('../models/user');
 const { requireFields, constructLocationHeader } = require('../utils/route-middleware');
 
 router.post('/users', requireFields(['fullName', 'username', 'password']), (req, res, next) => {
+  const { fullName, username, password } = req.body;
   const newUser = {
-    fullName: req.body.fullName,
-    username: req.body.username,
-    password: req.body.password
+    fullName,
+    username
   };
-  return User.create(newUser)
+  return User.hashPassword(password)
+    .then(digest => {
+      newUser.password = digest;
+      return User.create(newUser);
+    })
     .then(user => res
       .status(201)
       .location(constructLocationHeader(req, user))
