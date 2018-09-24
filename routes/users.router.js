7ced937d2acf13c4de8d1e648092a27bd02e8776
Morgan Newman <mpnewman95@gmail.com>
2018-09-24 12:11:48 -0700

    Integrates validation into POST /api/users

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const User = require('../models/user');
-const { requireFields, constructLocationHeader } = require('../utils/route-middleware');
+const { requireFields, validateUser, constructLocationHeader } = require('../utils/route-middleware');
 
-router.post('/users', requireFields(['fullName', 'username', 'password']), (req, res, next) => {
+router.post('/users', requireFields(['username', 'password'], 422), validateUser, (req, res, next) => {
   const { fullName, username, password } = req.body;
   const newUser = {
     fullName,
