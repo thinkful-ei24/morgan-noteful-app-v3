f7c5aa6f94d136555ad578b0da1cf9e047683eba
Morgan Newman <mpnewman95@gmail.com>
2018-09-28 09:37:29 -0700

    Fixes tags tests

--- test/tags.test.js
+++ test/tags.test.js
@@ -1,29 +1,48 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
 
 const app = require('../server');
-const {
-  TEST_MONGODB_URI
-} = require('../config');
+const { TEST_MONGODB_URI } = require('../config');
 
 const Tag = require('../models/tag');
-
 const { tags } = require('../db/data');
+const User = require('../models/user');
+const { users } = require('../db/data');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Tag Router Tests', () => {
   before(function() {
-    return mongoose.connect(TEST_MONGODB_URI, {
-        useNewUrlParser: true
-      })
+    return mongoose
+      .connect(
+        TEST_MONGODB_URI,
+        {
+          useNewUrlParser: true
+        }
+      )
       .then(() => mongoose.connection.db.dropDatabase());
   });
 
+  let token;
+  let user;
   beforeEach(function() {
-    return Tag.insertMany(tags).then(() => Tag.createIndexes());
+    return Promise.all(users.map(user => User.hashPassword(user.password)))
+      .then(digests => {
+        users.forEach((user, i) => (user.password = digests[i]));
+        return Promise.all([
+          User.insertMany(users),
+          Tag.insertMany(tags),
+          Tag.createIndexes()
+        ]);
+      })
+      .then(([users]) => {
+        user = users[0];
+        token = jwt.sign({ user }, JWT_SECRET, { subject: user.username });
+      });
   });
 
   afterEach(function() {
@@ -36,9 +55,8 @@ describe('Tag Router Tests', () => {
 
   const req = (method, endpoint) => {
     method = method.toLowerCase();
-    return chai.request(app)[method]('/api/tags' + endpoint);
+    return chai.request(app)[method]('/api/tags' + endpoint).set('Authorization', `Bearer ${token}`);
   };
-
 
   const validateFields = (res, expectedFields) => {
     const response = res.body;
@@ -50,48 +68,43 @@ describe('Tag Router Tests', () => {
       } else expect(response).to.have.keys(expectedFields);
     }
   };
-  const expectedFields = ['id', 'name', 'createdAt', 'updatedAt'];
+  const expectedFields = ['id', 'name', 'createdAt', 'updatedAt', 'userId'];
 
   describe('GET /api/tags', function() {
-
     it('should respond with all tags', () => {
       // 1) Call the database **and** the API
       // 2) Wait for both promises to resolve using `Promise.all`
-      return Promise.all([
-        Tag.find(),
-        req('get', '/')
-      ])
-        // 3) then compare database results to API response
-        .then(([data, res]) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(data.length);
-          validateFields(res, expectedFields);
-        });
+      return (
+        Promise.all([Tag.find({ userId: user.id }), req('get', '/')])
+          // 3) then compare database results to API response
+          .then(([data, res]) => {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('array');
+            expect(res.body).to.have.length(data.length);
+            validateFields(res, expectedFields);
+          })
+      );
     });
 
     it('should sort tags by name', () => {
-      return req('get', '/')
-        .then((dbRes) => {
-          expect(dbRes.body.map(item => item.name)).to.eql(['breed', 'domestic', 'feral', 'hybrid']);
-        });
+      return req('get', '/').then(dbRes => {
+        expect(dbRes.body.map(item => item.name)).to.eql(dbRes.body.map(item => item.name).sort());
+      });
     });
-
   });
 
   describe('GET /api/tags/:id', function() {
-
     it('should return correct tag by the `id` parameter', function() {
       let data;
       // 1) First, call the database
-      return Tag.findOne()
+      return Tag.findOne({ userId: user.id })
         .then(_data => {
           data = _data;
           // 2) then call the API with the ID
           return req('get', `/${data.id}`);
         })
-        .then((res) => {
+        .then(res => {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
 
@@ -109,58 +122,50 @@ describe('Tag Router Tests', () => {
     it('should return the expected fields', () => {
       // 1) First, call the database
       let data;
-      return Tag.findOne()
+      return Tag.findOne({ userId: user.id })
         .then(_data => {
           data = _data;
           // 2) then call the API with the ID
           return req('get', `/${data.id}`);
         })
-        .then((res) => {
+        .then(res => {
           validateFields(res, expectedFields);
         });
     });
 
     it('should make sure the `id` parameter is a valid ID', () => {
-      return req('get', '/INVALIDIDHERE')
-        .then(res => {
-          expect(res).to.have.status(400);
-        });
+      return req('get', '/INVALIDIDHERE').then(res => {
+        expect(res).to.have.status(400);
+      });
     });
 
     it('should 404 if the ID is valid but does not exist in the database', () => {
-      return req('get', '/faaaaaaaaaaaaaaaaaaaaaaa')
-        .then(res => {
-          expect(res).to.have.status(404);
-        });
+      return req('get', '/faaaaaaaaaaaaaaaaaaaaaaa').then(res => {
+        expect(res).to.have.status(404);
+      });
     });
-
   });
 
   describe('POST /api/tags', function() {
-    
     const newItem = {
       name: 'TestTag'
     };
-    
+
     it('should create and return a new item when provided valid data', function() {
       let res;
       // 1) First, call the API
-      return req('post', '/')
-        .send(newItem)
-        .then(function(_res) {
-          res = _res;
-          expect(res).to.have.status(201);
-          expect(res).to.have.header('location');
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('object');
-          validateFields(res, expectedFields);
-          // 2) then call the database
-          return Tag.findById(res.body.id);
-        })
-        // 3) then compare the API response to the database results
-        .then(data => {
-          expect(res.body.id).to.equal(data.id);
-        });
+      return (
+        req('post', '/')
+          .send(newItem)
+          .then(function(_res) {
+            res = _res;
+            expect(res).to.have.status(201);
+            expect(res).to.have.header('location');
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('object');
+            validateFields(res, expectedFields);
+          })
+      );
     });
 
     it('should return an object with the expected fields', () => {
@@ -174,13 +179,13 @@ describe('Tag Router Tests', () => {
 
     it('should catch duplicate key errors', () => {
       let item;
-      return Tag.findOne()
+      return Tag.findOne({ userId: user.id })
         .then(_data => {
           item = _data;
-          return chai.request(app).post('/api/tags/')
-            .send({name: item.name});
+          return req('post', '/')
+            .send({ name: item.name });
         })
-        .then((res) => {
+        .then(res => {
           expect(res).to.have.status(400);
         });
     });
@@ -189,24 +194,24 @@ describe('Tag Router Tests', () => {
       req('post', '/')
         .send(newItem)
         .then(res => {
-          expect(res).to.have.header('Location', /\/api\/tags\/[0-9a-fA-F]{24}/);
+          expect(res).to.have.header(
+            'Location',
+            /\/api\/tags\/[0-9a-fA-F]{24}/
+          );
         });
     });
-
   });
 
   describe('PUT /api/tags/:id', () => {
-
     it('should update a tag by an `id`', () => {
       let item;
-      return Tag.findOne()
+      return Tag.findOne({ userId: user.id })
         .then(res => {
           item = res;
-          return req('put', `/${item.id}`)
-            .send({
-              id: item.id,
-              name: 'testTag'
-            });
+          return req('put', `/${item.id}`).send({
+            id: item.id,
+            name: 'testTag'
+          });
         })
         .then(res => {
           validateFields(res, expectedFields);
@@ -257,40 +262,33 @@ describe('Tag Router Tests', () => {
           expect(res).to.have.status(400);
         });
     });
-
   });
 
   describe('DELETE /api/tags/:id', () => {
-
     it('should delete a tag by an `id`', () => {
-      return req('delete', '/222222222222222222222202')
-        .then(res => {
-          expect(res).to.have.status(204);
-          expect(res).to.not.have.key('Archive');
-        });
+      return Tag.findOne({ userId: user.id })
+        .then(res => req('delete', `/${res.id}`))
+        .then(res => expect(res).to.have.status(204));
     });
 
     it('should require a valid id', () => {
-      return req('delete', '/00000')
-        .then(res => {
-          expect(res).to.have.status(400);
-        });
-
+      return req('delete', '/00000').then(res => {
+        expect(res).to.have.status(400);
+      });
     });
 
     it('should 404 if the id is valid but does not exist in the database', () => {
-      return req('delete', '/faaaaaaaaaaaaaaaaaaaaaaa')
-        .then(res => {
-          expect(res).to.have.status(404);
-        });
+      return req('delete', '/faaaaaaaaaaaaaaaaaaaaaaa').then(res => {
+        expect(res).to.have.status(404);
+      });
     });
 
     it('should remove corresponding tagId references after deleting tag', () => {
       return req('delete', '222222222222222222222201')
-        .then((res) => {
-          return chai.request(app).get('/api/notes/');
+        .then(res => {
+          return chai.request(app).get('/api/notes/').set('Authorization', `Bearer ${token}`);
         })
-        .then((res) => {
+        .then(res => {
           const notesWithTag = res.body.reduce((tags, note) => {
             if (note.tags.includes('222222222222222222222201')) tags++;
             return tags;
@@ -298,7 +296,5 @@ describe('Tag Router Tests', () => {
           expect(notesWithTag).to.equal(0);
         });
     });
-
   });
-
 });
